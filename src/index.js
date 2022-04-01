faeff3555c8f4832dba0a4dc71f90898b9118884
Marcin Rudzki <mar.rudzki@gmail.com>
2022-04-01 06:58:11 +0200

    playing with destructuring of props and array

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const [, , third] = ["Alex", "Ali", "Anna"];
+
+console.log(third);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="Marcin" />
   </React.StrictMode>,
   document.getElementById('root')
 );
