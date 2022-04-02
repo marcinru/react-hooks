4ad282dff5de31f6b1d45d846e168850f80c5b62
Marcin Rudzki <mar.rudzki@gmail.com>
2022-04-02 06:43:51 +0200

    useState hook for status

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-const [, , third] = ["Alex", "Ali", "Anna"];
+function App() {
+  const [ status, setStatus ] = useState('Not delivered');
 
-console.log(third);
+  return (
+    <div>
+      <h1>The package is: {status}</h1>
+      <button onClick={() => setStatus('Delivered')}>Deliver</button>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name="Marcin" />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
